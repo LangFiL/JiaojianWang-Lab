9e86af7c196c7eebfc69acb80149ebd88a85e7ba
kustlpbr <1505681584liweia@gmail.com>
2024-03-03 20:43:29 +0800

    commit IDAC by weili

---
+++ toolbox/IDAC/README.md
@@ -0,0 +1,4 @@
+# Iso-Distant Average Correlation (IDAC) #
+
+
+**For more information, see https://doi.org/10.1016/j.neuroimage.2021.118150**
---
+++ toolbox/IDAC/script/mask_segment.m
@@ -0,0 +1,15 @@
+function mask_segment(mask_path,roi)
+    info = load_untouch_nii(mask_path);
+    img = info.img;
+    img(img > 0 & img <= roi/2) = 1;
+    img(img>roi/2) = 0;
+    info.img = img;
+    save_untouch_nii(info,'lh')
+    
+    info = load_untouch_nii(mask_path);
+    img = info.img;
+    img(img > 0 & img <= 300) = 0;
+    img(img>roi/2) = 1;
+    info.img = img;
+    save_untouch_nii(info,'rh')
+end
