1f4be5e8d9a7fcdf40bbe459bc87591de0b3a2fe
liwei <105126058+LangFiL@users.noreply.github.com>
2024-03-04 13:09:48 +0800

    Update mask_segment.m

--- toolbox/IDAC/script/mask_segment.m
+++ toolbox/IDAC/script/mask_segment.m
@@ -8,7 +8,7 @@ function mask_segment(mask_path,roi)
     
     info = load_untouch_nii(mask_path);
     img = info.img;
-    img(img > 0 & img <= 300) = 0;
+    img(img > 0 & img <= roi/2) = 0;
     img(img>roi/2) = 1;
     info.img = img;
     save_untouch_nii(info,'rh')
